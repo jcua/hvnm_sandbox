69cbfb2df37270bf5db428e6d26f977d55c838d1
Jonathan Cua <jonathan.cua@gmail.com>
2014-12-21 21:31:13 -0800

    limit number of commands that tcp_backend accepts

--- tcp_backend.pl
+++ tcp_backend.pl
@@ -4,33 +4,45 @@ use strict;
 use Mojo::IOLoop;
 
 my $delay = 0;
+my $current_time = 0;
+my $start_flag = 1;
+my $welcome = "Welcome to $0; type help for command list.\n";
+
 Mojo::IOLoop->server({port => 3010} => sub {
     my ($loop, $stream) = @_;
 
     $stream->on(read => sub {
         my ($stream, $method) = @_;
-        my $msg;
+        my $msg = '';
 
-        if ($method =~ 'start_rep') {
-            $msg = 'start replication';
-        }
-        elsif ($method =~ 'stop_rep') {
-            $msg = 'stop replication';
-        }
-        elsif ($method =~ 'start_delay') {
-            $msg = 'start_delay';
-            $delay = 1;
-        }
-        elsif ($method =~ 'stop_delay') {
-            $msg = 'start_delay';
-            $delay = 1;
-        }
-        elsif ($method =~ 'status') {
-            $msg = "delay is $delay";
+        if ($start_flag == 1) {
+            $stream->write($welcome);
+            $start_flag = 0;
         }
 
-        $stream->write($msg . "\n");
-        $delay++ if ($delay > 0);
+        if ($method =~ 'start') {
+            $msg = "delay has been started.\n";
+            $delay = 1;
+            $current_time = time();
+        }
+        elsif ($method =~ 'stop') {
+            $msg = "delay has been stopped\n";
+            $delay = 0;
+        }
+        elsif ($method =~ 'stat') {
+            my $time_diff = time() - $current_time;
+            if ($delay == 1) {
+                $msg = "delay is $time_diff.\n";
+            }
+            else {
+                $msg = "delay is 0.\n";
+            }
+        }
+        elsif ($method =~ 'help') {
+            $msg = "Available commands: start, stop, stat.\n";
+        }
+
+        $stream->write($msg);
     });
 });
 
