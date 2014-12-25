6abfe7b82536456ef5156445e377f3eec6f6e2af
Jonathan Cua <jonathan.cua@gmail.com>
2014-12-25 09:15:50 -0800

    make tcp_client.pl work in a non-interactive mode

--- tcp_client.pl
+++ tcp_client.pl
@@ -1,11 +1,19 @@
 #!/usr/bin/perl
 
 use strict;
+use Getopt::Long;
 use IO::Socket;
 
+GetOptions( 'listen=i' => \my $opt_port,
+            'exec=s'   => \my $opt_exec,
+            'h'        => \my $opt_help,
+);
+
+usage() if ( ! defined( $opt_port ) &&
+  ! defined( $opt_exec ) || defined( $opt_help ) );
+
 my $host   = '127.0.0.1';
-my ($port) = @ARGV;
-die "Usage: $0 port\n" unless (@ARGV == 1);
+my ($port) = $opt_port;
 
 my $socket = IO::Socket::INET->new(
         Proto    => 'tcp',
@@ -14,21 +22,37 @@ my $socket = IO::Socket::INET->new(
 ) or die "Could not create socket: $!";
 $socket->autoflush(1);
 
-my $welcome = "Welcome to $host:$port; type help for command list.\n";
-$welcome .= "Hit Ctrl-C to exit.\n";
-print STDERR $welcome;
-
-die "Cannot fork: $!" unless defined(my $kidpid = fork());
-
-if ($kidpid) {
-    while (my $line = <$socket>) {
-        print STDOUT $line;
-    }
-    kill("TERM", $kidpid)
+if ( $opt_exec ) {
+    print $socket $opt_exec;
+    my $line = <$socket>;
+    $line =~ s/^\s+//g;
+    print STDOUT $line;
+    exit;
 }
 else {
-    while (my $line = <STDIN>) {
-        print $socket $line;
+    my $welcome = "Welcome to $host:$port; type help for command list.\n";
+    $welcome .= "Hit Ctrl-C to exit.\n";
+    print STDERR $welcome;
+
+    die "Cannot fork: $!" unless defined(my $kidpid = fork());
+
+    if ($kidpid) {
+        while (my $line = <$socket>) {
+            print STDOUT $line;
+        }
+        kill("TERM", $kidpid);
+    }
+    else {
+        while (my $line = <STDIN>) {
+            print $socket $line;
+        }
     }
 }
 
+sub usage {
+    print "\n";
+    print qq{Usage: $0 -l <port>\n}
+        . qq{Usage: $0 -l <port> -e <start|stop|stat>\n\n};
+
+    exit;
+}
